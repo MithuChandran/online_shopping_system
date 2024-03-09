78b47dd7ac544f310780d5b73cacf5b4f2eace9d
mithuna <mithunachandrasekaran@gmail.com>
2024-03-09 16:57:17 +0530

    Add ShoppingManager interface with methods

---
+++ src/ShoppingManager.java
@@ -0,0 +1,8 @@
+public interface ShoppingManager {
+public void displayMenu();
+public void addProduct();
+public void deleteProduct();
+public void printList();
+public void loadList();
+public void saveList();
+}
